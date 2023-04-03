f013b86a3c82f0e038c4286e54fd1d84d1d3d3c3
Teme <temppase@gmail.com>
2023-04-03 13:57:51 +0300

    update

--- scripst.js
+++ scripst.js
@@ -1,7 +1,7 @@
 
 function displayData() {
-    document.getElementById("satList").reset();
-    document.getElementById("decList").reset();
+    document.getElementById("satList").value = null;
+    document.getElementById("decList").value = null;
     let mg = parseFloat(document.getElementById("mg").value.replace(",","."));
     let hlt =  parseInt(document.getElementById("hlt").value);
     let p = parseInt(document.getElementById("p").value);
